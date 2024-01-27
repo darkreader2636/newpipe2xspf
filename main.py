a9ad21e79b461768cbb8a8429a22af3f91ced16a
darkreader2636 <119101878+darkreader2636@users.noreply.github.com>
2024-01-27 16:10:28 +0300

    Working

--- main.py
+++ main.py
@@ -1,2 +1,85 @@
 import sqlite3
 from typing import Tuple, List
+from functools import wraps
+
+Playlist = Tuple[int, str, int, int]  # 0:Playlist ID 1:Playlist Name 2:is_thumbnail_permanent 3:thumb_stream_id
+
+Video = Tuple[int ,int,str,str,str, int,str,str ,str,int ,str,int,int]    # 0:URL 1:Title 2:Uploader #3:Playlist ID
+
+class db_wrapper:
+    def __init__(self):
+        self.open()
+
+    def open(self):
+        """Initializes the database"""
+        self.conn = sqlite3.connect('newpipe.db')
+        self.cur = self.conn.cursor()
+
+    def close(self):
+        """Safely closes the database"""
+        if self.conn:
+            self.conn.commit()
+            self.cur.close()
+            self.conn.close()
+
+    def _commit(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            result = func(self, *args, **kwargs)
+            self.conn.commit()
+            return result
+        return wrapper
+
+    def get_playlist(self) -> Playlist:
+        self.cur.execute(
+            "SELECT * FROM playlists",
+        )
+        result = self.cur.fetchall()
+        if result:
+            return result
+
+    def get_streams(self, list_id: int) -> list:
+        self.cur.execute(
+            "SELECT * FROM playlist_stream_join WHERE playlist_id=? ORDER BY join_index ASC",
+            (str(list_id),)
+        )
+        result = self.cur.fetchall()
+        if result:
+            return result
+
+    def get_video(self, uuid: int) -> Video:
+        self.cur.execute(
+            "SELECT * FROM streams WHERE uid=?",
+            (str(uuid),)
+        )
+        result = self.cur.fetchall()
+        if result:
+            return result
+
+def main():
+    db = db_wrapper()
+    i = 1
+
+    print("sel list:\n")
+    crt_list = db.get_playlist()
+    for pl_list in crt_list:
+        print(f"{i}:{pl_list[1]}")
+        i = i+1
+
+    selected = crt_list[int(input(">")) -1 ][0]
+    print(selected)
+
+    mangled = db.get_streams(selected)
+
+
+    my_list = []
+    for uid in mangled:
+        my_list.append(db.get_video(uid[1]))
+
+    for vid in my_list:
+        print(f"{vid[0][3]} {vid[0][2]}")
+
+    db.close()
+
+while True:
+    main()
